92c72a683482e4cc0a5732b192e3d77c30f02bed
YannUFLL <yannlescoul79@free.fr>
2026-03-12 01:09:55 +0100

    add: sepolia and etherscan to the hardhat config

--- code/hardhat.config.ts
+++ code/hardhat.config.ts
@@ -1,8 +1,23 @@
-import type { HardhatUserConfig } from "hardhat/config";
+import type { HardhatUserConfig} from "hardhat/config";
+import { vars} from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox-viem";
+
+const ownerKey = vars.get("OWNER_PRIVATE_KEY"); 
+const INFURA_API_KEY = vars.get("INFURA_API_KEY");
+
+
 
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
+  networks: {
+    sepolia: {
+      url: `https://sepolia.infura.io/v3/${INFURA_API_KEY}`,
+      accounts: [ownerKey],
+    },
+  },
+  etherscan: {
+  apiKey: vars.get("ETHERSCAN_API_KEY"), // Il te faudra une clé Etherscan gratuite
+  },
 };
 
 export default config;
